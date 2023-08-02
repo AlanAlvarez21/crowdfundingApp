f3f30f35d6f9d5a69d5fb6bbefee524d236350d3
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2023-08-01 19:55:31 -0600

    feat: add routes in App.jsx document

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -7,15 +7,17 @@ import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 const App = () => {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
-    <div className="relative hidden mr-10 sm:flex">
-      <Sidebar />
-    </div>
-
+      <div className="relative hidden mr-10 sm:flex">
+        <Sidebar />
+      </div>
+      
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-        <Navbar />
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create-campaign" element={<CreateCampaign />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/campaign-details/:id" element={<CampaignDetails />} />
       </Routes>
       </div>
     </div>
