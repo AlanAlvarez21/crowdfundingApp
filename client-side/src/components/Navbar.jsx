63eb819354754aae0249ed5ac939dfef8dc50319
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2023-08-03 14:23:34 -0600

    fix: some grid adjustments

--- client-side/src/components/Navbar.jsx
+++ client-side/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <div className="flex flex-col-reverse md:flex-row justify-between mb-[35px] gap-6">
-      <div className="lg:flex-1 flex flex-row py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
+      <div className="lg:flex-1 flex flex-row py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px] md:mt-[10px] sm:mt-[20px]">
         <input type="text" placeholder="Search for campaigns" className="flex w-full font-epilogue font-normal text[14px] placeholder:text-[#4b5263] text-white bg-transparent outline-none"/>
         <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
           <img src={search} alt="search" className="w-[15px] h-[15px] object-contain" />
