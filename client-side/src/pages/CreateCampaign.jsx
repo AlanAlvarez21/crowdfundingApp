68f752d7fc8cdce8326001e1eaa82e22ccdd1d8f
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2023-08-03 13:57:45 -0600

    feat: updated create campaign page

--- client-side/src/pages/CreateCampaign.jsx
+++ client-side/src/pages/CreateCampaign.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { ethers } from 'ethers';
 
 import { money } from '../assets'
-import { CustomButton } from '../components'
+import { CustomButton, FormField } from '../components'
 import { checkIfImage } from '../utils'
 
 const CreateCampaign = () => {
@@ -18,6 +18,10 @@ const CreateCampaign = () => {
     image: '',
   });
 
+  const handleSubmit = () => {
+
+  }
+
   return (
     <div className='bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4'>
       {isLoading && 'Loader...'}
@@ -26,6 +30,60 @@ const CreateCampaign = () => {
           Start a Campaign
         </h1>
       </div>
+
+      <form onSubmit={handleSubmit} className="w-full mt-[60px] flex flex-col gap-[30px]">
+        <div className="flex flex-wrap gap-[40px]">
+          <FormField 
+            labelName='Your Name *'
+            placeholder='Alan Alvarez'
+            inputType='text'
+            value={form.name}
+            handleChange={() => {}}
+          />
+          <FormField 
+            labelName='Campaign Title *'
+            placeholder='Write a title'
+            inputType='text'
+            value={form.title}
+            handleChange={() => {}}
+          />
+        </div>
+        <FormField 
+            labelName='Story *'
+            placeholder='Write your story'
+            isTextArea
+            value={form.description}
+            handleChange={() => {}}
+          />
+
+          <div className='flex items-center justify-start w-full p-4 bg-[#8c6dfd] rounded-[10px]'>
+            <img src={money} alt="money" className='w-[40px] h-[40px] object-contain'  />
+              <h4 className='font-epilogue font-bold text-[25px] text-white ml-[20px]' >You will get 100% of the raised amount</h4>
+          </div>
+          <div className="flex flex-wrap gap-[40px]">
+          <FormField 
+            labelName='Goal *'
+            placeholder='ETH 0.50'
+            inputType='text'
+            value={form.target}
+            handleChange={() => {}}
+          />
+          <FormField 
+            labelName='End Date *'
+            placeholder='End Date'
+            inputType='date'
+            value={form.deadline}
+            handleChange={() => {}}
+          />
+          <div className='flex justify-center flex-wrap items-center mt-[30px]'>
+            <CustomButton 
+              btnType="submit"
+              title="Submit new campaign"
+              styles="bg-[#1dc071]"
+            />
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
