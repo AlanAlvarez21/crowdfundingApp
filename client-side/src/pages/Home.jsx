eecba7ba75165206eedc9e7a9d973236ae8d392b
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2023-08-09 18:30:34 -0600

    feat: fetch campaigns from Home component

--- client-side/src/pages/Home.jsx
+++ client-side/src/pages/Home.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { useStateContext } from '../context'
+import { DisplayCampaigns } from '../components'
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+  const { address, contract, getCampaigns } = useStateContext();
+
+  const fetchCampaings = async () => {
+    setIsLoading(true);
+      const data = await getCampaigns();
+      setCampaigns(data);
+      console.log('data', data);
+      setIsLoading(false);
+  }
+
+  useEffect(() => { 
+    if(contract) fetchCampaings(); 
+  }, [address, contract])
+
   return (
-    <div>Home</div>
+    <DisplayCampaigns 
+      title='All campaings'
+      isLoading={isLoading}
+      campaings={campaigns}
+    />
   )
 }
 
