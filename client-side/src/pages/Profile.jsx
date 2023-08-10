942391d6982016d2aa6f62fc1aaa1517eb63a208
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2023-08-09 19:45:07 -0600

    feat: updated profile component to show only current user campaings

--- client-side/src/pages/Profile.jsx
+++ client-side/src/pages/Profile.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { useStateContext } from '../context'
+import { DisplayCampaigns } from '../components'
 
 const Profile = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+  const { address, contract, getUserCampaings } = useStateContext();
+
+  const fetchCampaings = async () => {
+    setIsLoading(true);
+      const data = await getUserCampaings();
+      setCampaigns(data);
+      console.log('data', data);
+      setIsLoading(false);
+  }
+
+  useEffect(() => { 
+    if(contract) fetchCampaings(); 
+  }, [address, contract])
+
   return (
-    <div>Profile</div>
+    <DisplayCampaigns 
+      title='All campaings'
+      isLoading={isLoading}
+      campaings={campaigns}
+    />
   )
 }
 
